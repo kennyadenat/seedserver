f3e5f769913c2b13ed9635a2edaf6d1d1dd2c2f8
Kehinde Adelugba <kennyadenat09@gmail.com>
2019-04-24 12:01:42 +0100

    42420191201

--- server/app.js
+++ server/app.js
@@ -67,7 +67,7 @@ app.use('/nasc', cors(), graphqlHTTP({
 }));
 
 //Mongoose Connection Setup
-mongoose.connect(keys.mongodb.prod, {
+mongoose.connect(keys.mongodb.dev, {
     promiseLibrary: require('bluebird'),
     useNewUrlParser: true
   })
--- server/config/key.js
+++ server/config/key.js
@@ -6,7 +6,9 @@ module.exports = {
   },
   mongodb: {
     url: "mongodb://localhost:27017/seedpro",
-    prod: "mongodb+srv://seedpro:Iremide-09@vigorlab-4gwpc.mongodb.net/seedpro?retryWrites=true"
+    prod: "mongodb+srv://seedpro:Iremide-09@vigorlab-4gwpc.mongodb.net/seedpro",
+    dev: "mongodb://seedpro:Iremide-09@vigorlab-shard-00-00-4gwpc.mongodb.net:27017,vigorlab-shard-00-01-4gwpc.mongodb.net:27017,vigorlab-shard-00-02-4gwpc.mongodb.net:27017/seedpro?ssl=true&replicaSet=Vigorlab-shard-0&authSource=admin&retryWrites=true",
+    dbName: "seedpro"
   },
   session: {
     cookieKey: "icanhehtyurjnghnk575768nnfhndvj4iunfurnnbg"
